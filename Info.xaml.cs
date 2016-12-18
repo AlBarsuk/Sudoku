fb0c59d962479a8f66e65cbbbcbb1c3f183b0ad1
Nastyadobrya <180897_nastya@mail.ru>
2016-12-18 18:32:54 +0300

    Create Info.xaml.cs

---
+++ Info.xaml.cs
@@ -0,0 +1,17 @@
+
+using System.Windows;
+
+
+namespace MySudoku
+{
+    /// <summary>
+    /// Логика взаимодействия для Info.xaml
+    /// </summary>
+    public partial class Info : Window
+    {
+        public Info()
+        {
+            InitializeComponent();
+        }
+    }
+}
