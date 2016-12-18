7b73eb62b379dc9cc0ba9cf75c4de916ed3f1518
Nastyadobrya <180897_nastya@mail.ru>
2016-12-18 18:03:22 +0300

    Update and rename Levels.xaml to Levels.xaml.cs

--- Levels.xaml.cs
+++ Levels.xaml.cs
@@ -1,16 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MySudoku
 {
@@ -27,7 +17,11 @@ namespace MySudoku
             InitializeComponent();
             
             g.mass = g.Create();
-        }
+            var mass = g.mass;
+            var k = mass;
+
+            EndButton.IsEnabled = false;
+        } 
         
         private void Show_cells(int?[][] massiv)
         {
@@ -58,17 +52,28 @@ namespace MySudoku
             current = g.mass;
             g.Easy(current);
             Show_cells(current);
+            EndButton.IsEnabled = true;
+            
+            checkBox2.IsEnabled = false;
+            checkBox1.IsEnabled = false;
         }
 
         private void checkBox1_Checked(object sender, RoutedEventArgs e)
         {
             current = g.mass;
             g.Medium(current);
+            EndButton.IsEnabled = true;
+           
+            checkBox2.IsEnabled = false;
+            checkBox.IsEnabled = false;
         }
 
         private void checkBox2_Checked(object sender, RoutedEventArgs e)
         {
-
+            current = g.mass;
+            g.Hard(current);
+            EndButton.IsEnabled = true;
+            
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
